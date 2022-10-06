d4d33c67c50c1313ba9bdc9c8adea8e5d7aa425c
Narcoss38 <crouzet.lilian@gmail.com>
2022-10-06 15:01:32 +0200

    FeedFragment implementation

--- app/src/main/java/fr/neon/e_kfc_v2/ui/time/TimeFragment.kt
+++ app/src/main/java/fr/neon/e_kfc_v2/ui/time/TimeFragment.kt
@@ -1,12 +1,15 @@
 package fr.neon.e_kfc_v2.ui.time
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import fr.neon.e_kfc_v2.R
 import fr.neon.e_kfc_v2.databinding.FragmentTimeBinding
 
 class TimeFragment : Fragment() {
@@ -23,11 +26,40 @@ class TimeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(TimeViewModel::class.java)
+            ViewModelProvider(this)[TimeViewModel::class.java]
 
         _binding = FragmentTimeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        ArrayAdapter.createFromResource(
+            requireActivity().applicationContext,
+            R.array.hours_morning,
+            android.R.layout.simple_spinner_item
+        ) .also { morningAdapter ->
+            morningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerLundiOpen.adapter = morningAdapter
+            binding.spinnerMardiOpen.adapter = morningAdapter
+            binding.spinnerMercrediOpen.adapter = morningAdapter
+            binding.spinnerJeudiOpen.adapter = morningAdapter
+            binding.spinnerVendrediOpen.adapter = morningAdapter
+            binding.spinnerSamediOpen.adapter = morningAdapter
+            binding.spinnerDimancheOpen.adapter = morningAdapter
+        }
+
+        ArrayAdapter.createFromResource(
+            requireActivity().applicationContext,
+            R.array.hours_afternoon,
+            android.R.layout.simple_spinner_item
+        ) .also { afternoonAdapter ->
+            afternoonAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerLundiClose.adapter = afternoonAdapter
+            binding.spinnerMardiClose.adapter = afternoonAdapter
+            binding.spinnerMercrediClose.adapter = afternoonAdapter
+            binding.spinnerJeudiClose.adapter = afternoonAdapter
+            binding.spinnerVendrediClose.adapter = afternoonAdapter
+            binding.spinnerSamediClose.adapter = afternoonAdapter
+            binding.spinnerDimancheClose.adapter = afternoonAdapter
+        }
 
         return root
     }
